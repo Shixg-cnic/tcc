9fb8805fa480c81e947313007ffaf5844558a8b5
XingguoShi <XingguoShi@users.noreply.github.com>
2026-09-20 16:05:05 +0800

    重构

--- include/swtcf_convert.cuh
+++ include/swtcf_convert.cuh
@@ -1,6 +1,6 @@
 #pragma once
 
-#include "format.hpp"
+#include "types.h"
 
 #include <cuda_runtime.h>
 
---
+++ include/types.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <cstdint>
+#include <vector>
+#include <limits>
+namespace tcc {
+
+using IndexType = std::uint32_t;
+using OffsetType = std::uint32_t;
+using ValueType = float;
+using BitmapType = std::uint64_t;
+
+}  // namespace tcc
