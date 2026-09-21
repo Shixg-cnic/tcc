63d64cb1ba86f38fdfca9fb818687111c8d0c0ad
XingguoShi <XingguoShi@users.noreply.github.com>
2026-09-21 10:58:10 +0800

    1

--- include/swtcf_spmm.cuh
+++ include/swtcf_spmm.cuh
@@ -2,7 +2,7 @@
 
 #include "swtcf_convert.cuh"
 #include <cuda_runtime.h>
-
+#include <cstdint>
 
 constexpr IndexType SWTCF_FEATURE_DIM = 128;
 constexpr IndexType SWTCF_DEFAULT_TC_THRESHOLD = 12;
@@ -15,4 +15,12 @@ void launchSWTCFSpMM(
     cudaStream_t stream = 0
 );
 
-
+void launchSWTCFSpMMQuantized(
+    const DeviceSWTCFMatrix& matrix,
+    const std::int8_t* quantizedX,
+    const ValueType* scales,
+    const IndexType* srcGlobal,
+    ValueType* matrixY,
+    IndexType tcThreshold = SWTCF_DEFAULT_TC_THRESHOLD,
+    cudaStream_t stream = 0
+);
